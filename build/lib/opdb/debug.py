29006b83ab7100e1a32edcf2ae4b3ff194c07f86
st0n3 <ssst0n3@gmail.com>
2022-09-28 22:42:35 +0800

    remove redundant

--- build/lib/opdb/debug.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from opdb import db
-from opdb.util import load_code_object
-
-filename = sys.argv[1]
-code_object = load_code_object(filename)
-db.run(code_object, dict(globals()), dict(locals()))
--- build/lib/opdb/test_opdb.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class Test(TestCase):
-    def test_load_code_object(self):
-        self.fail()
